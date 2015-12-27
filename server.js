26148d38d98ca6531e692ed57f1cf5a303af52c5
Tom Johnson <hertzz@users.noreply.github.com>
2015-12-27 14:46:39 +1100

    changed names

---
+++ server.js
@@ -0,0 +1,18 @@
+var path = require('path');
+global.appRoot = path.resolve(__dirname);
+
+var config = require(appRoot + '/conf/server');
+var util = require(appRoot + '/lib/httpUtils');
+var http = require('http');
+var server = http.createServer(util.handleRequest);
+
+var serverListenAddress = config.server.address || 'localhost';
+var serverListenPort = config.server.port || 80;
+
+server.listen(serverListenPort, serverListenAddress, function(){
+  console.log(
+    "\nServer listening on: http://%s:%s\n"+
+    "\n--- DATE ---------- TIME -----------------"+
+    "---- IP --------------- REQUEST ---",
+    serverListenAddress, serverListenPort);
+});
